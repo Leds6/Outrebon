95c8a5d47d77df99987ceb23c6137e1e99a9e937
Maxime <maxime.pereira@hei.yncrea.fr>
2018-11-27 14:21:51 +0100

    testChange

---
+++ var/cache/prod/smarty/compile/59/86/3e/59863ebd70d2cd6c45e90cae1c924f7bafd4b22e_0.file.popup.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-11-27 12:54:13
+  from 'D:\laragon\www\Boucherie\prestashop\modules\welcome\views\templates\popup.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bfd30659ff9b6_60392792',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '59863ebd70d2cd6c45e90cae1c924f7bafd4b22e' => 
+    array (
+      0 => 'D:\\laragon\\www\\Boucherie\\prestashop\\modules\\welcome\\views\\templates\\popup.tpl',
+      1 => 1543318886,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bfd30659ff9b6_60392792 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="onboarding-popup bootstrap">
+  <div class="content"></div>
+</div>
+<?php }
+}
